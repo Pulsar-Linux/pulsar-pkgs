e3f62c61f4a743f4b2d18402050ab178a49bff22
Celestia Ludenberg <ash8820@proton.me>
2026-06-12 19:44:40 +0200

    add .gitignore for build artifacts

--- packages/cnchi/pkg/cnchi/usr/share/cnchi/po/update_po.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-find . -name "*.po" | while read i;
-do
-   msgmerge "$i" cnchi.pot -o new.po;
-   mv new.po "$i";
-done
--- packages/cnchi/pkg/cnchi/usr/share/cnchi/utils/compile_all.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python -m compileall /usr/share/cnchi
